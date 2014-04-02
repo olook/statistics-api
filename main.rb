d48b44393c674582cfad6f2ed19660f5bb7274e0
Rafael <rafael.manoel@gmail.com>
2014-04-02 16:58:43 -0300

    avoid CORS error

--- main.rb
+++ main.rb
@@ -5,10 +5,17 @@ require './log_entry.rb'
 
 Mongoid.load!("mongoid.yml")
 
+options '/' do
+  headers['Access-Control-Allow-Origin'] = "*"
+  headers['Access-Control-Allow-Methods'] = "POST"
+  headers['Access-Control-Allow-Headers'] ="Content-type"
+end
+
+
 post '/' do
   params = JSON.parse(request.env["rack.input"].read)
-
   log = LogEntry.new(params)
+
   if log.save
     status 200
   else
