1dce6fbf0214785b8493e2806c81c4df2f5084f5
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-04-03 19:44:20 -0300

    Adding workers

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,4 @@
-pidfile File.expand_path(File.join(File.dirname(__FILE__), "../pid/puma.pid"))
-state_path File.expand_path(File.join(File.dirname(__FILE__), "../pid/puma_state"))
+pidfile File.expand_path(File.join(File.dirname(__FILE__), "../tmp/puma.pid"))
+state_path File.expand_path(File.join(File.dirname(__FILE__), "../tmp/puma_state"))
+workers 8
 activate_control_app
