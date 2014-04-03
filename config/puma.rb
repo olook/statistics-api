6c234ca4f4d3c4c243ea8cd5636badf5ccfc4d37
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-04-03 20:11:45 -0300

    Adding log option

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,5 @@
 pidfile File.expand_path(File.join(File.dirname(__FILE__), "../tmp/puma.pid"))
 state_path File.expand_path(File.join(File.dirname(__FILE__), "../tmp/puma_state"))
 workers 8
+stdout_redirect File.expand_path(File.join(File.dirname(__FILE__), "../log/puma.log")), File.expand_path(File.join(File.dirname(__FILE__), "../log/puma_err.log"))
 activate_control_app
