ef3f474613e673b80fae9933a07c70a135ccb1e6
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-04-03 19:03:58 -0300

    Adding config puma

---
+++ config/puma.rb
@@ -0,0 +1,3 @@
+pidfile File.expand_path(File.join(File.dirname(__FILE__), "../pid/puma.pid"))
+state_path File.expand_path(File.join(File.dirname(__FILE__), "../pid/puma_state"))
+activate_control_app
